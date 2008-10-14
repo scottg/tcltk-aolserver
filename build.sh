1cd666500ebc028e348a132a01b10f7022545509
Scott S. Goodwin <scott@scottg.net>
2008-10-14 14:29:38 -0400

    Build scripts and sources.

---
+++ build.sh
@@ -0,0 +1,34 @@
+#!/bin/bash -e
+
+# Copyright 2008 by Scott S. Goodwin
+
+if [ ! -f "config.sh" ]; then
+    echo "You aren't in the directory where build.sh is."
+    exit 1
+fi
+
+source config.sh
+
+/bin/mkdir -p exe
+/bin/mkdir -p var/db
+
+#
+# Unpack and build each source package.
+#
+
+for SOURCE in $SOURCES; do
+    cd $SITEROOT/src
+	NAME=`echo $SOURCE | cut -f1 -d'-'`
+	VERSION=`echo $SOURCE | cut -f2 -d'-'`
+    if [ -d $SITEROOT/exe/$NAME ]; then
+        echo "$SOURCE already installed in $SITEROOT/exe/$NAME"
+        continue
+    fi
+    echo "$SOURCE - compiling"
+    /bin/rm -rf $SOURCE
+    echo "tar xjf ${SOURCE}.tar.bz2 -- $PWD"
+    tar xjf ${SOURCE}.tar.bz2
+    cd $SITEROOT/src/$SOURCE
+	source $SITEROOT/make/$NAME
+done
+
---
+++ config.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+
+if [ "$(id -un)" == "root" ]; then
+    echo "You cannot run $0 as root"
+    exit 1
+fi
+
+if [ ! -f "config.sh" ]; then
+    echo "You aren't in the directory where build.sh is."
+    exit 1
+fi
+
+#
+# Site Settings
+#
+
+export SITEROOT=$PWD
+export SITENAME="tcltk"
+export SITEDESC="Tcl/Tk Conference 2008"
+export SITEOWNER=$(/usr/bin/id -un)
+export SITEGROUP=$(/usr/bin/id -gn)
+
+
+#
+# Tcl Settings
+#
+
+export TCLBIN=$SITEROOT/exe/tcl/bin
+export TCLSH_CMD="$TCLBIN/tclsh8.4"
+export TCLLIBPATH="$SITEROOT/exe/tcl/lib/tcl8.4 $SITEROOT/exe/tcl/lib"
+
+#
+# AOLserver Settings
+#
+
+export NSROOT=$SITEROOT/exe/aolserver
+
+export NS_HTTPPORT=8080
+export NS_HTTPSPORT=8443
+export NS_HTTPSPORT_PKI=8444
+export NS_ADDRESS=127.0.0.1
+export NS_HOSTNAME=localhost
+export NS_HOSTNAME=$NS_ADDRESS
+export NS_SERVERLOG=$SITEROOT/nsd.log
+export NS_ACCESSLOG=$SITEROOT/access.log
+export NS_MAILHOST=smtp.hq.nasa.gov
+export NS_MODULES=$SITEROOT/modules
+export NS_PAGES=$SITEROOT/pages
+
+#
+# PostgreSQL Settings
+#
+
+export PGBIN=$SITEROOT/exe/postgresql/bin
+export PGSHARE=$SITEROOT/exe/postgresql/share
+
+export PGHOST=
+export PGPORT=5432
+export PGDBNAME=$SITENAME
+export PGDATA=$SITEROOT/var/db
+
+export PSQLARGS="--set ON_ERROR_STOP=1"
+    # "--quiet"
+    # "--no-align"
+    # "--echo-all"
+
+#
+# Library and Manual Path Settings
+#
+
+
+
+#
+# Sources to Compile and Install
+#
+
+export SOURCES="
+    readline-5.2a
+    tcl-8.4.19
+    openssl-0.9.8i
+    postgresql-8.3.4
+    sqlite-3.6.3
+    aolserver-cvs
+    nspostgres-cvs
+    nsopenssl-cvs
+    nssqlite3-cvs
+"
