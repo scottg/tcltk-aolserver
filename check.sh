b37a4f1e04b7a83ce0394e2a4ed8260e85abe22f
Scott Goodwin <scott@scottg.net>
2008-10-16 10:14:35 -0400

    This script just tells you if any of the files in the expected list it holds are missing. A quick sanity check after a build.

---
+++ check.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Copyright 2008 by Scott S. Goodwin
+
+if [ ! -f "config.sh" ]; then
+    echo "You aren't in the directory where build.sh is."
+    exit 1
+fi
+
+source config.sh
+
+#
+# Confirm that all files are in place. Files are prefixed later with
+# $SITEROOT/exe.
+#
+
+FILES="
+	aolserver/bin/nsd
+	aolserver/bin/nsopenssl.so
+	aolserver/bin/nspostgres.so
+	aolserver/bin/nssqlite3.so
+	postgresql/bin/initdb
+	tcl/bin/tclsh8.4
+"
+
+for FILE in $FILES; do
+	if [ ! -e "$SITEROOT/exe/$FILE" ]; then
+		echo "FAILED: $FILE is not in place"
+	fi
+done
+
