4186fc9e7fc57e6be4980e7b4781dafedf347862
Scott S. Goodwin <scott@scottg.net>
2008-12-01 00:17:45 -0500

    Removed the tcltk site as it is identical to the 'default' site.

--- tcltk/config.sh
+++
@@ -1,9 +0,0 @@
-#
-# Specific configuration for this particular web site. These may override some
-# of what is in site-config.sh
-#
-
-export SITENAME=tcltk
-export SITEDESC="Tcl/Tk Conference 2008"
-
-export PGDBNAME=tcltk
