23188c9deaa63761aaf89ea78847465fb47708ee
Scott S. Goodwin <scott@scottg.net>
2008-12-01 19:54:32 -0500

    This config file was loading another copy of nsdb.
    It was already loaded by nsd.tcl; AOLserver should probably not allow
    multiple loading of modules.

--- config/nssqlite3.tcl
+++ config/nssqlite3.tcl
@@ -1,22 +1,20 @@
 #
- # SQLite v3 -- nssqlite3
- #
- ns_section  "ns/server/${servername}/modules"
- ns_param    nsdb            nsdb.so
+# SQLite v3 -- nssqlite3
+#
 
- ns_section  "ns/server/${servername}/db"
- ns_param    defaultpool     sqlite3
- ns_param    pools           *
+ns_section  "ns/server/${servername}/db"
+ns_param    defaultpool     sqlite3
+ns_param    pools           *
 
- ns_section  "ns/db/drivers"
- ns_param    sqlite3         nssqlite3.so
+ns_section  "ns/db/drivers"
+ns_param    sqlite3         nssqlite3.so
 
- ns_section  "ns/db/pools"
- ns_param    sqlite3         "sqlite3"
+ns_section  "ns/db/pools"
+ns_param    sqlite3         "sqlite3"
 
- ns_section  "ns/db/pool/sqlite3"
- ns_param    driver          sqlite3
- ns_param    connections     1
- ns_param    datasource      /tmp/sqlite3.db
- ns_param    verbose         off
+ns_section  "ns/db/pool/sqlite3"
+ns_param    driver          sqlite3
+ns_param    connections     1
+ns_param    datasource      /tmp/sqlite3.db
+ns_param    verbose         off
 
